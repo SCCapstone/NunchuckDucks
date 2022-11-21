cd8fc0ec4c65f1d1724a6a2b051a57b1ce1f3cd7
Bryson2809 <jbc2@email.sc.edu>
2022-11-21 15:04:04 -0500

    initial commit, opens #134

---
+++ src/screens/CreateGoalScreen.js
@@ -0,0 +1,7 @@
+import { Text, View, StyleSheet } from "react-native";
+import { grayThemeColor } from "../library/constants";
+import { BackButton } from "../components/BackButton";
+
+export function CreateGoalScreen({}) {
+    
+}
