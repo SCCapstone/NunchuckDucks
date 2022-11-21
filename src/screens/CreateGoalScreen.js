32f2714422a5ded9268c5159a04bbf0c1ce55491
Bryson2809 <jbc2@email.sc.edu>
2022-11-21 16:49:07 -0500

    Updates #134

--- src/screens/CreateGoalScreen.js
+++ src/screens/CreateGoalScreen.js
@@ -1,7 +1,43 @@
 import { Text, View, StyleSheet } from "react-native";
 import { grayThemeColor } from "../library/constants";
-import { BackButton } from "../components/BackButton";
+import BackButton from "../components/BackButton";
+import CustomButton from "../components/CustomButton";
 
 export function CreateGoalScreen({}) {
     
+
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+const sampleGoalsList = [
+    {
+        goalType: "cardio",
+        goalDescription: "Run a 5 minute mile",
+    },
+    {
+        goalType: "Weight Lifting",
+        goalDescription: "225lb bench press PR",
+    },
+    {
+        goalType: "Weight",
+        goalDescription: "Weigh 185lbs by end of year",
+    },
+    {
+        goalType: "Weight Lifting",
+        goalDescription: "405lb squat PR",
+    },
+];
+
+const CreateGoalHeader = () => {
+    <View>
+        <BackButton />
+    </View>
 }
