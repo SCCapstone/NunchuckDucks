d9102394d2cf0456ce2ba3f9d17f253f3ab39059
kfairey11 <77368958+kfairey11@users.noreply.github.com>
2022-11-28 13:57:29 -0500

    added console logging to getAuthenticatedUser

--- src/library/GetAuthenticatedUser.js
+++ src/library/GetAuthenticatedUser.js
@@ -3,5 +3,8 @@ import { Auth } from "aws-amplify";
 
 export async function getCurrentAuthenticatedUser() {
     const { attributes } = await Auth.currentAuthenticatedUser();
-      return attributes.preferred_username;
+      const username = attributes.preferred_username;
+
+      console.log(`This happened with username ${username}`);
+      return username;
 }
