0cdb08f5870c055815dead18f0b5957b8a4c6cc9
Tyler Barrett <tylertb@email.sc.edu>
2022-11-28 13:02:02 -0500

    Connected GoalSummary to backend

--- src/components/GoalSummary/GoalSummary.js
+++ src/components/GoalSummary/GoalSummary.js
@@ -1,69 +1,83 @@
-import { View, Text, StyleSheet} from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Auth } from "aws-amplify";
+
+import { getGoals } from "../../crud/GoalOperations";
 import { blueThemeColor, grayThemeColor } from "../../library/constants";
-import React from "react";
 
+const GoalSummary = () => {
+  const [goals, setGoals] = useState([]);
 
-const GoalSummary = ({goal}) => {
-    return (
-            <View style={styles.container}>
-                <View style={styles.title}>
-                  <Text style= {styles.title}>Your Goals</Text>
-                </View>
+  async function goalList() {
+    const { attributes } = await Auth.currentAuthenticatedUser();
+    let username = attributes.preferred_username;
+    const goals = await getGoals(username);
+    setGoals(goals);
+  }
 
-                <View style = {styles.goaltextcontainer}>
-                  <Text style = {styles.goaltitle}>Goal 1: </Text>
-                  <Text style = {styles.goaltext}>@{goal}This is a placeholder</Text>
-                </View>
+  useEffect(() => {
+    goalList();
+  }, []);
 
-                <View style = {styles.goaltextcontainer}>
-                  <Text style = {styles.goaltitle}>Goal 2: </Text>
-                  <Text style = {styles.goaltext}>@{goal}This is a placeholder to show expansion and overflow with text </Text>
-                </View>
-                
-                <View style = {styles.goaltextcontainer}>
-                  <Text style = {styles.goaltitle}>Goal 3: </Text>
-                  <Text style = {styles.goaltext}>@{goal}This is a placeholder to easily visualize the box</Text>
-                </View>
-            </View>
-    );
-  };
+  const listGoals = goals.slice(0, 3).map((goal, index) => (
+    <View style={styles.goaltextcontainer}>
+      <Text style={styles.goaltitle}>Goal {index + 1}: </Text>
+      <Text style={styles.goaltext}>{goal.content}</Text>
+    </View>
+  ));
 
-  const styles = StyleSheet.create({
-    container: {
-      width: 310,
-      backgroundColor: "white",
-      borderColor: blueThemeColor,
-      borderWidth: 4.5,
-      minHeight: "auto",
-      borderRadius: 10,
-      flexDirection: "column",
-    },
-    goaltext: {
-      fontSize: 19,
-      maxWidth: 229
-    },
-    goaltextcontainer: {
-      paddingTop: 5,
-      flexDirection:"row",
-      textAlign: "left"
-    },
-    goaltitle:{
-      fontSize:19,
-      paddingLeft:5,
-      fontWeight: "600"
-    },
-    title: {
-        textAlign: "center",
-        width: "auto", //this changes length of line/ box the text exits on
-        fontSize: 22,
-        paddingTop: 2,
-        height: 33,
-        borderTopRightRadius:7,//curves top right
-        borderTopLeftRadius:7,//curves top left
-        color: blueThemeColor,
-        fontWeight: "bold",
-        backgroundColor: grayThemeColor,
-    },
-  });
+  const defaultForNoGoals = (
+    <View style={styles.goaltextcontainer}>
+      <Text style={styles.goaltitle}>No Goals Found</Text>
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.title}>
+        <Text style={styles.title}>Your Goals</Text>
+      </View>
+      {listGoals.length === 0 ? defaultForNoGoals : listGoals}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: 310,
+    backgroundColor: "white",
+    borderColor: blueThemeColor,
+    borderWidth: 4.5,
+    minHeight: "auto",
+    borderRadius: 10,
+    flexDirection: "column",
+  },
+  goaltext: {
+    fontSize: 19,
+    maxWidth: 229,
+  },
+  goaltextcontainer: {
+    paddingTop: 5,
+    flexDirection: "row",
+    textAlign: "left",
+  },
+  goaltitle: {
+    fontSize: 19,
+    paddingLeft: 5,
+    fontWeight: "600",
+  },
+  title: {
+    textAlign: "center",
+    width: "auto", //this changes length of line/ box the text exits on
+    fontSize: 22,
+    paddingTop: 2,
+    height: 33,
+    borderTopRightRadius: 7, //curves top right
+    borderTopLeftRadius: 7, //curves top left
+    color: blueThemeColor,
+    fontWeight: "bold",
+    backgroundColor: grayThemeColor,
+  },
+});
 
 export default GoalSummary;
