fe2138ff64d15fedc6b3ec0edc509a2bdc81600d
Bryson2809 <jbc2@email.sc.edu>
2023-01-22 22:45:57 -0500

    Initial commit opens #180

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,16 +1,17 @@
 import { View, Text } from "react-native";
 import { BackButton } from "../components/BackButton";
+import Header from "../components/Header/Header";
 export function SettingsScreen({navigation}) {
   return (
     <View
       style={{
         flex: 1,
-        alignItems: "center",
-        backgroundColor: "white",
-        justifyContent: "center",
+        alignItems: "center"
       }}
     >
-      <Text>This is the settings screen</Text>
+      <Header title={"Settings"} />
+
+      <Text>Toggle private account eventually goes here</Text>
     </View>
   );
 }
