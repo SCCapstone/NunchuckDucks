37ec7032c9e8a81aa382d382f593aad2857f62f9
Bryson2809 <jbc2@email.sc.edu>
2023-02-18 19:09:12 -0500

    Working on debugging

--- src/screens/NotificationsScreen.js
+++ src/screens/NotificationsScreen.js
@@ -1,25 +1,31 @@
 import React from "react";
 import { View, TextInput, Button } from "react-native";
-import { API, graphqlOperation, Auth } from "aws-amplify";
+import { API, graphqlOperation, Auth, DataStore } from "aws-amplify";
 import { Notifications } from "expo";
 import * as mutations from "../graphql/mutations.js";
 import * as queries from "../graphql/queries.js";
 import * as Permissions from "expo-permissions";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { User } from "../models";
 
 export function NotificationsScreen() {
+  useEffect(() => {
+    componentDidMount();
+  })
+
   const [Message, setMessage] = useState("");
   const [Profile, setProfile] = useState({})
-  const [User, setUser] = useState("")
+  const [currUser, setCurrUser] = useState({})
 
   async function componentDidMount() {
     const user = await Auth.currentSession()
       .then(data => {
-        this.setUser(data.idToken.payload.sub);
+        this.setCurrUser(data.idToken.payload.sub);
         return data.idToken.payload.sub;
       })
       .catch(err => console.log(err));
-    const profile = await this.getUserProfile(user);
+    const profile = await getUserProfile(currUser);
+    console.log("Profile: " + profile);
     // There is no expoToken available yet, so we will request that and save it into the profile
     if (profile.expoToken === null) {
       const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
@@ -31,9 +37,10 @@ export function NotificationsScreen() {
       // Only update the profile with the expoToken if it not exists yet
       if (token !== "") {
         const inputParams = {
-          id: user,
+          id: currUser,
           expoToken: token
         };
+        console.log("These are the input params: " + inputParams);
         await API.graphql(
           graphqlOperation(mutations.updateUser, { input: inputParams })
         )
@@ -45,25 +52,32 @@ export function NotificationsScreen() {
     }
   }
   async function getUserProfile(sub) {
-    const result = await API.graphql(
-      graphqlOperation(queries.getUser, { id: sub })
-    )
-      .then(result => {
-        this.setProfile(result.data.getUser);
-        return result.data.getUser;
-      })
+    // const result = await API.graphql(
+    //   graphqlOperation(queries.getUser, { id: sub })
+    // )
+    const result = await DataStore.query(User,sub)
+      // .then(result => {
+      //   this.setProfile(result.data.getCurrUser);
+      //   return result.data.getCurrUser;
+      // })
       .catch(err => console.log(err));
     return result;
   }
 
   async function handleSubmit() {
+
     const inputParams = {
       message: Message,
       token: Profile.expoToken,
       name: Profile.name,
       email: Profile.email,
-      id: User
+      id: currUser
     };
+    console.log("This is the input params message: " + inputParams.message);
+    // console.log("Profole Expo Token: " + inputParams.Profile.expoToken);
+    // console.log("Name: " + inputParams.Profile.name);
+    // console.log("Email: " + inputParams.Profile.email);
+    // console.log("Id: " + inputParams.id);
     await API.graphql(
       graphqlOperation(mutations.pinpoint, { input: inputParams })
     )
@@ -73,7 +87,7 @@ export function NotificationsScreen() {
         this.setState({ message: "" });
       })
       .catch(err => console.log(err));
-      console.log("Success");
+      //console.log("Success");
   }
 
   return (
