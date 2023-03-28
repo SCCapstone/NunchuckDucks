ea32f09cb73ce453799c82aa64c58f47ba8f3025
Cort Miles <cwmiles@email.sc.edu>
2023-03-27 20:45:41 -0400

    Settings and Mutual Both White now.

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -19,6 +19,8 @@ export function SettingsScreen({ navigation }) {
     },
     container: {
       display: "flex",
+      height: "100%",
+      backgroundColor:"white"
     },
     text: {
       top: 33,
