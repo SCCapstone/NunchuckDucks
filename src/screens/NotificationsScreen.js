e31497fcb90e2269e522ca1c4701c6e038293ac8
kfairey11 <77368958+kfairey11@users.noreply.github.com>
2023-04-18 21:55:45 -0400

    #388 adds default text when there are no notifications

--- src/screens/NotificationsScreen.js
+++ src/screens/NotificationsScreen.js
@@ -33,11 +33,14 @@ export function NotificationsScreen() {
   const currUser = getCurrentUser();
   const [notifications, setNotifications] = useState([]);
   const [forceRefresh, setForceRefresh] = useState(true);
+  const [isEmpty, setIsEmpty] = useState(false);
 
   async function notificationList() {
     const username = await getCurrentUser();
     const Notifications = await getNotifications(username);
     setNotifications(Notifications);
+    if (Notifications.length === 0)
+        setIsEmpty(true);
   }
 
   async function deleteAllButton(username) {
@@ -77,7 +80,14 @@ export function NotificationsScreen() {
             title={"Notifications"}
             style={{ backgroundColor: "white" }}
           />
+          { isEmpty && (
+            <View>
+                <Text style={styles.defaultText}>Looks like there are no new notifications!</Text>
+            </View>
+          )}
+          { !isEmpty && (
           <CustomButton buttonType={"default"} text={"Delete All"} onClick={() => deleteAllButton(currUser)}></CustomButton>
+          )}
         </View>
         <View>
           <ScrollView contentContainerStyle={styles.scroll}>
@@ -103,4 +113,9 @@ const styles = StyleSheet.create({
     width: 100,
     margin: 10,
   },
+  defaultText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    paddingTop: 30
+  }
 });
