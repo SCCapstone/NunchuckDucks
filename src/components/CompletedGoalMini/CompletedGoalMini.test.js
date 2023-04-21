d24408590a37ddffb1a3dc0b212d0266a2a77d25
Bryson2809 <jbc2@email.sc.edu>
2023-04-20 23:37:18 -0400

    Add new test searching for text

--- src/components/CompletedGoalMini/CompletedGoalMini.test.js
+++ src/components/CompletedGoalMini/CompletedGoalMini.test.js
@@ -1,8 +1,16 @@
 import * as React from "react";
 import renderer from "react-test-renderer";
+import { screen } from "@testing-library/react";
 import CompletedGoalMini from "./CompletedGoalMini";
+
+let text = "test";
 
 it(`renders correctly`, () => {
   const tree = renderer.create(<CompletedGoalMini></CompletedGoalMini>);
   expect(tree).toBeDefined();
+});
+
+it(`renders text`, () => {
+  const goal = renderer.create(<CompletedGoalMini description={text}></CompletedGoalMini>);
+  expect(screen.findByText("test")).toBeInDocument();
 });
