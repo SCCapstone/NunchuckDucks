73af11f6fed6bcb6a74b3193dc7a9a3f1fb8e968
Tyler Barrett <tylertb@email.sc.edu>
2023-04-21 21:11:45 -0400

    Requested Changes

--- src/components/Workout/Workout.js
+++ src/components/Workout/Workout.js
@@ -10,7 +10,7 @@ export default function Workout({ workout, isAbsolute = false }) {
   }
   let JsonWorkout = workout;
   try {
-    if (typeof JsonWorkot === "string") JsonWorkout = JSON.parse(JsonWorkout);
+    if (typeof JsonWorkout === "string") JsonWorkout = JSON.parse(JsonWorkout);
   } catch (error) {
     console.log("Workout Parsing: ", error);
     JsonWorkout = workout;
