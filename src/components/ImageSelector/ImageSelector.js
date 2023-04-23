d47567ea1c6be3cebac9cb68f90efa7f8656c07b
nathandolbir <75330713+nathandolbir@users.noreply.github.com>
2023-04-22 23:27:00 -0400

    changed thing

--- src/components/ImageSelector/ImageSelector.js
+++ src/components/ImageSelector/ImageSelector.js
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
     flex: 1,
     //opacity: 0.7,
     //alignItems: "center",
-    padding: 10,
+    paddingTop: 5,
     justifyContent: "center",
     //right: 0,
     //bottom: 0,
